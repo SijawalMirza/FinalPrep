8704083b64b05039186951b22c0f271b9b6052e2
SijawalMirza <sijawalayoub@gmail.com>
2018-01-08 00:25:19 +0500

    Sql

---
+++ app/src/main/java/com/example/sijaw/finalprep/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.sijaw.finalprep;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+
+
+public class MainActivity extends Activity {
+    String TAG="MTAG";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        MyDb myDb=new MyDb(this);
+        myDb.insertdata(5);
+        Log.d(TAG, "onCreate: "+myDb.getdata());
+    }
+}
